3d57f47904709cb1d558e0ba70a052146278ea34
stepforwardtech <jrstrayhorn@gmail.com>
2018-01-31 14:28:05 -0500

    setting up webpack, getting react/react-dom working!!

---
+++ app/index.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+require('./index.css');
+
+function App() {
+  return <div>Hello World</div>;
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
