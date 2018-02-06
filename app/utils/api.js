1ac9228bac1f4eb35a5d060f125feb2379e3176a
stepforwardtech <jrstrayhorn@gmail.com>
2018-02-06 06:50:40 -0500

    Refactoring to ES6

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,4 +1,4 @@
-var axios = require('axios');
+const axios = require('axios');
 
 function handleError(error) {
   console.warn(error);
@@ -8,32 +8,29 @@ function handleError(error) {
 function getWeatherData(location) {
   return axios
     .get(
-      'https://api.opencagedata.com/geocode/v1/json?q=' +
-        location +
-        '&key=50a0ea95055e4f0ab5f85b5e3ef9fa00'
+      `https://api.opencagedata.com/geocode/v1/json?q=${location}&key=50a0ea95055e4f0ab5f85b5e3ef9fa00`
     )
-    .then(function(res) {
-      var locData = {
-        city: res.data.results[0].components.city,
-        state: res.data.results[0].components.state,
-        lat: res.data.results[0].geometry.lat,
-        lng: res.data.results[0].geometry.lng
+    .then(({ data: { results } }) => {
+      const {
+        components: { city, state },
+        geometry: { lat, lng }
+      } = results[0];
+      const locData = {
+        city: city,
+        state: state,
+        lat: lat,
+        lng: lng
       };
 
       return axios
         .get(
-          'https://api.openweathermap.org/data/2.5/weather?' +
-            'lat=' +
-            locData.lat +
-            '&lon=' +
-            locData.lng +
-            '&APPID=224b7da19b9e9c30983d4c504b9c491e'
+          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&APPID=224b7da19b9e9c30983d4c504b9c491e`
         )
-        .then(function(res) {
-          locData['temp'] = convertToFahrenheit(res.data.main.temp);
-          locData['description'] = capitalizeFirstLetter(
-            res.data.weather[0].description
-          );
+        .then(({ data }) => {
+          const { main: { temp } } = data;
+          const { description } = data.weather[0];
+          locData['temp'] = convertToFahrenheit(temp);
+          locData['description'] = capitalizeFirstLetter(description);
 
           return locData;
         });
